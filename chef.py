5c1fa0ee776814b349997a367ddde0610e33c9bc
Diego Lorden <djlorden@gmail.com>
2014-02-23 18:03:10 -0800

    integrate chef and parser

--- chef.py
+++ chef.py
@@ -1,7 +1,22 @@
 from tts import TTS
+from recipe_parser import RecipeParser
+
+
 if __name__ == '__main__':
     tts = TTS()
+    rp = RecipeParser('icecreamcone.json')
     text = raw_input('Say:')
     while text:
-        tts.say(text)
+        if text == 'ingredients':
+            ingredients = rp.get_ingredients()
+            for ingredient in ingredients:
+                tts.say(ingredient)
+        elif text == 'instructions':
+            instructions = rp.get_instruction()
+            for instruction in instructions:
+                tts.say(instruction[0:100])
+        elif rp.find_ingredient(text):
+            ingredient = rp.find_ingredient(text)[0]
+            tts.say(ingredient['amount'] + ' ' + ingredient['ingredient'])
+
         text = raw_input('Say:')
