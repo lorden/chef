f75b8f139e97dece9649d968810d1ed400fb8c3f
Diego Lorden <djlorden@gmail.com>
2014-02-23 17:03:37 -0800

    remove unused comment

--- tts.py
+++ tts.py
@@ -10,7 +10,6 @@ class TTS:
         self.temp_file = '/tmp/chef.mp3'
 
     def get_file(self, path):
-        # urllib.urlretrieve(path, self.temp_file)
         with open(self.temp_file, 'wb') as handle:
             r = requests.get(path, stream=True)
             for block in r.iter_content(1024):
